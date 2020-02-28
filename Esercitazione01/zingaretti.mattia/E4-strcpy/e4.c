4134f1a598854a5d05cf25d92878f66d3f5c2ac3
zingarettimattia <zingaretti.mattia@gmail.com>
2020-02-28 16:29:54 +0000

    Fix segmentation fault

--- Esercitazione01/zingaretti.mattia/E4-strcpy/e4.c
+++ Esercitazione01/zingaretti.mattia/E4-strcpy/e4.c
@@ -1,9 +1,11 @@
 char * my_strcpy(char *dest, const char *src){
-	char* start = dest;
-	while(*src || *dest){
+	char* start = dest;	
+	while(*src){
+		printf("Da 0x%x a 0x%x, vals: %c : %c (0x%x)\n",src,dest,*src,*dest,*dest);
 		*dest = *src;
 		src++;
 		dest++;
 	}
+	*dest = 0; 
 	return start;
 }
